10681b44c8538657a10a9ab26e4e679fab3fac61
Jonathan-9717 <201195385+Jonathan-9717@users.noreply.github.com>
2025-04-10 18:09:26 -0700

    Complete playGame function
    Finished playGame function which plays 5 rounds.
    At the end of the fifth round console.log the
    winner based on total score of the 5 rounds.

--- scripts.js
+++ scripts.js
@@ -5,8 +5,7 @@
 let randomNum;
 let computerChoice;
 let humanChoice;
-let computerScore = 0;
-let humanScore = 0;
+
 
 function getComputerChoice() {
     randomNum = Math.floor(Math.random() * 3 + 1);
@@ -19,9 +18,7 @@ function getComputerChoice() {
     };
 };
 
-getComputerChoice();
-console.log(randomNum);
-console.log(computerChoice);
+
 
 // Create a new function getHumanChoice that will returm one of the valid choices depending on what the user inputs
 
@@ -29,31 +26,56 @@ function getHumanChoice() {
     humanChoice = prompt("Choose: Rock, Paper, or Scissors");
 };
 
-getHumanChoice();
-console.log(humanChoice);
+
 
 // Create new function playRound that take two parameters, humanChoice and computerChoice
 
-function playRound(computerChoice, humanChoice) {
-    // Make parameters case-incensitve
-    let lowercaseComputerChoice = computerChoice.toLowerCase();
-    let lowercaseHumanChoice = humanChoice.toLowerCase();
 
-    // playRound function should console.log a string value such as: "You lose! Paper beats Rock".
-    if ((lowercaseComputerChoice === "rock" && lowercaseHumanChoice === "scissors") || (lowercaseComputerChoice === "paper" && lowercaseHumanChoice === "rock") || (lowercaseComputerChoice === "scissors" && lowercaseHumanChoice === "paper")) {
-        console.log(`You lose ${lowercaseComputerChoice} beats ${lowercaseHumanChoice}`);
-        computerScore ++;
-    } else if((lowercaseHumanChoice === "rock" && lowercaseComputerChoice === "scissors") || (lowercaseHumanChoice === "paper" && lowercaseComputerChoice === "rock") || (lowercaseHumanChoice === "scissors" && lowercaseComputerChoice === "paper")) {
-        console.log(`You win! ${lowercaseHumanChoice} beats ${lowercaseComputerChoice}`)
-        humanScore ++;
+
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
+    for(let i =1; i <= 5; i++) {
+        function playRound(computerChoice, humanChoice) {
+            // Make parameters case-incensitve
+            let lowercaseComputerChoice = computerChoice.toLowerCase();
+            let lowercaseHumanChoice = humanChoice.toLowerCase();
+        
+            // playRound function should console.log a string value such as: "You lose! Paper beats Rock".
+            if ((lowercaseComputerChoice === "rock" && lowercaseHumanChoice === "scissors") || (lowercaseComputerChoice === "paper" && lowercaseHumanChoice === "rock") || (lowercaseComputerChoice === "scissors" && lowercaseHumanChoice === "paper")) {
+                console.log(`You lose this round! ${lowercaseComputerChoice} beats ${lowercaseHumanChoice}`);
+                computerScore ++;
+            } else if((lowercaseHumanChoice === "rock" && lowercaseComputerChoice === "scissors") || (lowercaseHumanChoice === "paper" && lowercaseComputerChoice === "rock") || (lowercaseHumanChoice === "scissors" && lowercaseComputerChoice === "paper")) {
+                console.log(`You win this round! ${lowercaseHumanChoice} beats ${lowercaseComputerChoice}`)
+                humanScore ++;
+            } else {
+                console.log("Draw");
+            }
+        
+            // Increment the humanScore or computerScore
+            console.log(`Human: ${humanScore} vs Computer: ${computerScore}`);
+        
+        };
+
+        console.log(`Round ${i}`);
+
+        getComputerChoice();
+        // console.log(randomNum);
+        // console.log(computerChoice);
+
+        getHumanChoice();
+        // console.log(humanChoice);
+        
+        playRound(computerChoice, humanChoice);
+    };
+
+    if (humanScore > computerScore) {
+        console.log(`You won the Game! ${humanScore} to ${computerScore}`);
+    } else if (computerScore > humanScore) {
+        console.log(`You lost ${computerScore} to ${humanScore} :(. Try again!`);
+    } else {
+        console.log("Tied Game");
     }
-
-    // Increment the humanScore or computerScore
-    console.log(`Human: ${humanScore} vs Computer: ${computerScore}`);
-
 };
 
-// Game will play 5 rounds. Create function name playGame that calls playRound to play 5 rounds, keeps scores and declares a winner at the end.
-
-// Move playRound function and score variables so that they're declared inside of the new playGame function
-
+playGame();
